cc0cb1e45d89a2a3a2e39846eba4a035df285390
ElectricBubble <bubble_yang@yeah.net>
2019-04-08 00:36:42 +0800

    优化packr后的文件（使用sed删除不必要的.go文件）

--- doPackr.sh
+++ doPackr.sh
@@ -5,3 +5,8 @@ datasetsPath=${basePath}/datasets
 cd $GOPATH/src/github.com/gobuffalo/packr/packr
 go run main.go -i ${templatesPath}
 go run main.go -i ${datasetsPath}
+
+templatesPackrFile=$(cd ${templatesPath}; ls | grep "\-packr.go")
+datasetsPackrFile=$(cd ${datasetsPath}; ls | grep "\-packr.go")
+sed -i '' '/.go\"/d' ${templatesPath}/${templatesPackrFile}
+sed -i '' '/.go\"/d' ${datasetsPath}/${datasetsPackrFile}
