cece109ce02c0c5fa1acd8fc90ec5c37b3aba2a8
chenjiandongx <chenjiandongx@qq.com>
2020-11-04 15:15:55 +0800

    Fix CI test

--- charts/wordcloud_test.go
+++ charts/wordcloud_test.go
@@ -9,12 +9,12 @@ import (
 
 func TestWordCloudAssetsBeforeRender(t *testing.T) {
 	wc := NewWordCloud()
-	assert.Equal(t, []string{"echarts.min.js"}, wc.JSAssets.Values)
+	assert.Equal(t, []string{"echarts.min.js", "echarts-wordcloud.min.js"}, wc.JSAssets.Values)
 }
 
 func TestWordCloudAssetsAfterRender(t *testing.T) {
 	wc := NewWordCloud()
 	err := wc.Render(os.Stdout)
 	assert.NoError(t, err)
-	assert.Equal(t, []string{host + "echarts.min.js"}, wc.JSAssets.Values)
+	assert.Equal(t, []string{host + "echarts.min.js", host + "echarts-wordcloud.min.js"}, wc.JSAssets.Values)
 }
