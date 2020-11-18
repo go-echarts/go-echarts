e94e70afa6e0bc9edc8dfe7013f61c5a95171530
evangwt <evangwt@gmail.com>
2020-11-18 15:16:41 +0700

    Remove unused constraints

--- opts/global.go
+++ opts/global.go
@@ -77,10 +77,6 @@ func setField(field reflect.Value, defaultVal string) {
 }
 
 const (
-	//	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	//	letterIdxBits = 6                    // 6 bits to represent a letter index
-	//	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	//	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 	chartIDSize = 12
 )
 
