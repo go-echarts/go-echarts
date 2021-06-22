41ad47a7a24f943df1f7279ede0501f3be93d23f
Rocky Yang <martinyangrq@outlook.com>
2021-06-22 09:43:38 +0800

    * Added comment to AxisTick fields

--- opts/global.go
+++ opts/global.go
@@ -548,7 +548,16 @@ type AxisLabel struct {
 }
 
 type AxisTick struct {
-	Show     bool   `json:"show"`
+	// Set this to false to prevent the axis tick from showing.
+	Show bool `json:"show"`
+
+	// interval of axisTick, which is available in category axis. is set to be the same as axisLabel.interval by default.
+	// It uses a strategy that labels do not overlap by default.
+	// You may set it to be 0 to display all labels compulsively.
+	// If it is set to be 1, it means that labels are shown once after one label. And if it is set to be 2, it means labels are shown once after two labels, and so on.
+	// On the other hand, you can control by callback function, whose format is shown below:
+	// (index:number, value: string) => boolean
+	// The first parameter is index of category, and the second parameter is the name of category. The return values decides whether to display label.
 	Interval string `json:"interval,omitempty"`
 }
 
