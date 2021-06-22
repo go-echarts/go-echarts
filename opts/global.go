2aa5b208e8179e269e8727b6c94d5af5b3644a7a
Rocky Yang <martinyangrq@outlook.com>
2021-06-22 10:27:41 +0800

    Added MinInterval and MaxInterval option for line (#186)
    * Added ShowSymbol option for line
    
    * Added MinInterval and MaxInterval option for line
    
    * * Added AxisTick option to XAxis
    * AxisLabel.Show should not be omitempty as the default echarts value is true
    
    * * Added AxisLine option to YAxis
    
    * * Added comment to AxisTick fields
    
    * * Added comments
    
    * * omitempty to prevent array index out of bound problem
    
    Co-authored-by: dongdong <chenjiandongx@qq.com>

--- opts/global.go
+++ opts/global.go
@@ -475,7 +475,7 @@ type ToolBoxFeatureRestore struct {
 // https://echarts.apache.org/en/option.html#xAxis.axisLabel
 type AxisLabel struct {
 	// Set this to false to prevent the axis label from appearing.
-	Show bool `json:"show,omitempty"`
+	Show bool `json:"show"`
 
 	// Interval of Axis label, which is available in category axis.
 	// It uses a strategy that labels do not overlap by default.
@@ -547,6 +547,49 @@ type AxisLabel struct {
 	LineHeight string `json:"lineHeight,omitempty"`
 }
 
+type AxisTick struct {
+	// Set this to false to prevent the axis tick from showing.
+	Show bool `json:"show"`
+
+	// interval of axisTick, which is available in category axis. is set to be the same as axisLabel.interval by default.
+	// It uses a strategy that labels do not overlap by default.
+	// You may set it to be 0 to display all labels compulsively.
+	// If it is set to be 1, it means that labels are shown once after one label. And if it is set to be 2, it means labels are shown once after two labels, and so on.
+	// On the other hand, you can control by callback function, whose format is shown below:
+	// (index:number, value: string) => boolean
+	// The first parameter is index of category, and the second parameter is the name of category. The return values decides whether to display label.
+	Interval string `json:"interval,omitempty"`
+}
+
+// AxisLine controls settings related to axis line.
+// https://echarts.apache.org/en/option.html#yAxis.axisLine
+type AxisLine struct {
+	// Set this to false to prevent the axis line from showing.
+	Show bool `json:"show"`
+
+	// Specifies whether X or Y axis lies on the other's origin position, where value is 0 on axis.
+	// Valid only if the other axis is of value type, and contains 0 value.
+	OnZero bool `json:"onZero,omitempty"`
+
+	// When multiple axes exists, this option can be used to specify which axis can be "onZero" to.
+	OnZeroAxisIndex int `json:"onZeroAxisIndex,omitempty"`
+
+	// Symbol of the two ends of the axis. It could be a string, representing the same symbol for two ends; or an array
+	// with two string elements, representing the two ends separately. It's set to be 'none' by default, meaning no
+	//arrow for either end. If it is set to be 'arrow', there shall be two arrows. If there should only one arrow
+	//at the end, it should set to be ['none', 'arrow'].
+	Symbol string `json:"symbol,omitempty"`
+
+	// Size of the arrows at two ends. The first is the width perpendicular to the axis, the next is the width parallel to the axis.
+	SymbolSize []float64 `json:"symbolSize,omitempty"`
+
+	// Arrow offset of axis. If is array, the first number is the offset of the arrow at the beginning, and the second
+	// number is the offset of the arrow at the end. If is number, it means the arrows have the same offset.
+	SymbolOffset []float64 `json:"symbolOffset,omitempty"`
+
+	LineStyle *LineStyle `json:"lineStyle,omitempty"`
+}
+
 // XAxis is the option set for X axis.
 // https://echarts.apache.org/en/option.html#xAxis
 type XAxis struct {
@@ -593,6 +636,12 @@ type XAxis struct {
 	// It will be automatically computed to make sure axis tick is equally distributed when not set.
 	Max interface{} `json:"max,omitempty"`
 
+	// Minimum gap between split lines. For 'time' axis, MinInterval is in unit of milliseconds.
+	MinInterval float64 `json:"minInterval,omitempty"`
+
+	// Maximum gap between split lines. For 'time' axis, MaxInterval is in unit of milliseconds.
+	MaxInterval float64 `json:"maxInterval,omitempty"`
+
 	// The index of grid which the x axis belongs to. Defaults to be in the first grid.
 	// default 0
 	GridIndex int `json:"gridIndex,omitempty"`
@@ -605,6 +654,9 @@ type XAxis struct {
 
 	// Settings related to axis label.
 	AxisLabel *AxisLabel `json:"axisLabel,omitempty"`
+
+	// Settings related to axis tick.
+	AxisTick *AxisTick `json:"axisTick,omitempty"`
 }
 
 // YAxis is the option set for Y axis.
@@ -665,6 +717,9 @@ type YAxis struct {
 
 	// Settings related to axis label.
 	AxisLabel *AxisLabel `json:"axisLabel,omitempty"`
+
+	// Settings related to axis line.
+	AxisLine *AxisLine `json:"axisLine,omitempty"`
 }
 
 // TextStyle is the option set for a text style component.
