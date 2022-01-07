88ba515a03e7ad13e1d854c9cda28cf18c97bf36
l-we <liuwanle2010@gmail.com>
2022-01-07 11:06:04 +0800

    add left,right,top and bottom to opts.VisualMap https://echarts.apache.org/zh/option.html#visualMap-piecewise.left

--- opts/global.go
+++ opts/global.go
@@ -796,6 +796,30 @@ type VisualMap struct {
 
 	// Define visual channels that will mapped from dataValues that are in selected range.
 	InRange *VisualMapInRange `json:"inRange,omitempty"`
+
+	// Distance between visualMap component and the left side of the container.
+	// left value can be instant pixel value like 20; it can also be a percentage
+	// value relative to container width like '20%'; and it can also be 'left', 'center', or 'right'.
+	// If the left value is set to be 'left', 'center', or 'right',
+	// then the component will be aligned automatically based on position.
+	Left string `json:"left,omitempty"`
+
+	// Distance between visualMap component and the right side of the container.
+	// right value can be instant pixel value like 20; it can also be a percentage
+	// value relative to container width like '20%'.
+	Right string `json:"right,omitempty"`
+
+	// Distance between visualMap component and the top side of the container.
+	// top value can be instant pixel value like 20; it can also be a percentage
+	// value relative to container width like '20%'; and it can also be 'top', 'middle', or 'bottom'.
+	// If the left value is set to be 'top', 'middle', or 'bottom',
+	// then the component will be aligned automatically based on position.
+	Top string `json:"top,omitempty"`
+
+	// Distance between visualMap component and the bottom side of the container.
+	// bottom value can be instant pixel value like 20; it can also be a percentage
+	// value relative to container width like '20%'.
+	Bottom string `json:"bottom,omitempty"`
 }
 
 // VisualMapInRange is a visual map instance in a range.
