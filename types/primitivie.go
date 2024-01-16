477a2ecdac10043a7a584e2c8c05003a709d2dd5
Koy Zhuang <koy@ko8e24.top>
2024-01-16 20:23:46 +0800

    chore: polish (#336)

--- types/primitivie.go
+++ types/primitivie.go
@@ -1,6 +1,11 @@
 package types
 
+// Bool a wrapper type of *bool, use opts.Bool to simply convert it.
 type Bool *bool
 type Int *int
 type Float *float32
 type String string
+
+// FuncStr a pure JavaScrip function string or special formatted string
+// use opts.FuncOpts or opts.FuncStripCommentsOpts to embed JavaScript.
+type FuncStr string
