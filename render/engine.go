b8c1502394a24f96de4800c4b7e42fad6ca08a1c
koyzhuang <koy@ko8e24.top>
2024-01-20 00:17:37 +0800

    update: separate render

--- render/engine.go
+++ render/engine.go
@@ -1,7 +1,6 @@
 package render
 
 import (
-	"bytes"
 	"fmt"
 	"html/template"
 	"io"
@@ -9,16 +8,8 @@ import (
 	"regexp"
 	"strings"
 
-	tpls "github.com/go-echarts/go-echarts/v2/templates"
 	"github.com/go-echarts/go-echarts/v2/types"
 )
-
-// Renderer
-// Any kinds of charts have their render implementation and
-// you can define your own render logic easily.
-type Renderer interface {
-	Render(w io.Writer) error
-}
 
 const (
 	ModChart = "chart"
@@ -31,64 +22,11 @@ const (
 
 var pat = regexp.MustCompile(`(__f__")|("__f__)|(__f__)`)
 
-type pageRender struct {
-	c      interface{}
-	before []func()
-}
-
-// NewPageRender returns a render implementation for Page.
-func NewPageRender(c interface{}, before ...func()) Renderer {
-	return &pageRender{c: c, before: before}
-}
-
-// Render renders the page into the given io.Writer.
-func (r *pageRender) Render(w io.Writer) error {
-	for _, fn := range r.before {
-		fn()
-	}
-
-	contents := []string{tpls.HeaderTpl, tpls.BaseTpl, tpls.PageTpl}
-	tpl := MustTemplate(ModPage, contents)
-
-	var buf bytes.Buffer
-	if err := tpl.ExecuteTemplate(&buf, ModPage, r.c); err != nil {
-		panic(err)
-	}
-
-	content := pat.ReplaceAll(buf.Bytes(), []byte(""))
-
-	_, err := w.Write(content)
-	return err
-}
-
-type chartRender struct {
-	c      interface{}
-	before []func()
-}
-
-// NewChartRender returns a render implementation for Chart.
-func NewChartRender(c interface{}, before ...func()) Renderer {
-	return &chartRender{c: c, before: before}
-}
-
-// Render renders the chart into the given io.Writer.
-func (r *chartRender) Render(w io.Writer) error {
-	for _, fn := range r.before {
-		fn()
-	}
-
-	contents := []string{tpls.HeaderTpl, tpls.BaseTpl, tpls.ChartTpl}
-	tpl := MustTemplate(ModChart, contents)
-
-	var buf bytes.Buffer
-	if err := tpl.ExecuteTemplate(&buf, ModChart, r.c); err != nil {
-		return err
-	}
-
-	content := pat.ReplaceAll(buf.Bytes(), []byte(""))
-
-	_, err := w.Write(content)
-	return err
+// Renderer
+// Any kinds of charts have their render implementation and
+// you can define your own render logic easily.
+type Renderer interface {
+	Render(w io.Writer) error
 }
 
 // isSet check if the field exist in the chart instance
