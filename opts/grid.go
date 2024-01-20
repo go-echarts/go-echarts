91c4b22a4de1131e1d90b69025a812bceeb122ef
Koy Zhuang <koy@ko8e24.top>
2024-01-20 19:03:02 +0800

    update: opts for grid sync in echarts (#353)

--- opts/grid.go
+++ opts/grid.go
@@ -5,14 +5,17 @@ import "github.com/go-echarts/go-echarts/v2/types"
 // Grid Drawing grid in rectangular coordinate.
 // https://echarts.apache.org/en/option.html#grid
 type Grid struct {
+	// Whether to show the grid in rectangular coordinate.
+	Show types.Bool `json:"show,omitempty"`
+
 	// Distance between grid component and the left side of the container.
 	Left string `json:"left,omitempty"`
 
-	// Distance between grid component and the right side of the container.
-	Right string `json:"right,omitempty"`
-
 	// Distance between grid component and the top side of the container.
 	Top string `json:"top,omitempty"`
+
+	// Distance between grid component and the right side of the container.
+	Right string `json:"right,omitempty"`
 
 	// Distance between grid component and the bottom side of the container.
 	Bottom string `json:"bottom,omitempty"`
@@ -20,8 +23,10 @@ type Grid struct {
 	// Width of grid component.
 	Width string `json:"width,omitempty"`
 
-	ContainLabel types.Bool `json:"containLabel,omitempty"`
-
 	// Height of grid component. Adaptive by default.
 	Height string `json:"height,omitempty"`
+
+	ContainLabel types.Bool `json:"containLabel,omitempty"`
+
+	Tooltip *Tooltip `json:"tooltip,omitempty"`
 }
