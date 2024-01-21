4e91a27d06122cbadad5e386c178fa0cba774d59
Koy Zhuang <koy@ko8e24.top>
2024-01-21 16:17:55 +0800

    update: expose series fully configurable (#359)

--- charts/series.go
+++ charts/series.go
@@ -128,6 +128,15 @@ type SingleSeries struct {
 
 type SeriesOpts func(s *SingleSeries)
 
+type SingleSeriesOperator func(s *SingleSeries)
+
+// WithSeriesOpts If the WithXXX helper method is not good enough, use this directly!
+func WithSeriesOpts(ssr SingleSeriesOperator) SeriesOpts {
+	return func(s *SingleSeries) {
+		ssr(s)
+	}
+}
+
 func WithCoordinateSystem(cs string) SeriesOpts {
 	return func(s *SingleSeries) {
 		s.CoordSystem = cs
