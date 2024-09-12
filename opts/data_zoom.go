934617119849401558ebea8d8c79112613d00e03
Koy Zhuang <koy@ko8e24.top>
2024-09-12 21:00:27 +0800

    update(opt): add FilterMode for data zoom (#483)

--- opts/data_zoom.go
+++ opts/data_zoom.go
@@ -58,4 +58,14 @@ type DataZoom struct {
 	//     return 'aaa' + value + 'bbb';
 	// }
 	LabelFormatter string `json:"labelFormatter,omitempty"`
+
+	// FilterMode Generally dataZoom component zoom or roam coordinate system
+	// https://echarts.apache.org/en/option.html#dataZoom-inside.filterMode
+	// through data filtering and set the windows of axes internally.
+	// Possible values:
+	//'filter': data that outside the window will be filtered, which may lead to some changes of windows of other axes. For each data item, it will be filtered if one of the relevant dimensions is out of the window.
+	//'weakFilter': data that outside the window will be filtered, which may lead to some changes of windows of other axes. For each data item, it will be filtered only if all of the relevant dimensions are out of the same side of the window.
+	//'empty': data that outside the window will be set to NaN, which will not lead to changes of windows of other axes.
+	//'none': Do not filter data.
+	FilterMode string `json:"filterMode,omitempty"`
 }
