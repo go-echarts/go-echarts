a4a209d27d49e4de66d8eeedc1282786194f107f
sunyihao <sunyihoo@163.com>
2024-11-04 17:02:02 +0800

    update(opt): Add new fields to the TextStyle/Progress/Detail structure.

--- opts/charts.go
+++ opts/charts.go
@@ -529,14 +529,133 @@ type CustomChart struct {
 
 // Progress is the options set for progress.
 type Progress struct {
-	// Wether to show the progress
+	// Whether to show the progress, default is false.
 	Show types.Bool `json:"show,omitempty"`
+
+	// Whether the progress overlaps when there are multiple groups of data, default is true.
+	OverLap types.Bool `json:"overlap,omitempty"`
+
 	// Width of the progress in px
 	Width int `json:"width,omitempty"`
+
+	// Whether to add round caps at the end, default is false.
+	RoundCap types.Bool `json:"roundCap,omitempty"`
+
+	// Whether to clip overflow, default is false.
+	Clip types.Bool `json:"clip,omitempty"`
+
+	// The style of progress.
+	ItemStyle *ItemStyle `json:"itemStyle,omitempty"`
 }
 
 // Detail is the options set for detail (e.g. on a gauge).
 type Detail struct {
+	// Whether to show the details, default is true.
+	Show types.Bool `json:"show,omitempty"`
+
+	// Font color
+	Color string `json:"color,omitempty"`
+
+	// Font style
+	// Options: 'normal', 'italic', 'oblique'
+	FontStyle string `json:"fontStyle,omitempty"`
+
+	// FontWeight main title font thick weight.
+	// Options are:
+	// 'normal'
+	// 'bold'
+	// 'bolder'
+	// 'lighter'
+	// 100 | 200 | 300 | 400...
+	FontWeight string `json:"fontWeight,omitempty"`
+
+	// Font family the main title font family.
+	// Options: "sans-serif", 'serif' , 'monospace', 'Arial', 'Courier New', 'Microsoft YaHei', ...
+	FontFamily string `json:"fontFamily,omitempty"`
+
+	// Font size of the value in px
+	FontSize int `json:"fontSize,omitempty"`
+
+	// Line height of the text fragment.
+	LineHeight int `json:"lineHeight,omitempty"`
+
+	// Background color of label, which is transparent by default.
+	BackgroundColor string `json:"backgroundColor,omitempty"`
+
+	// Border color of label.
+	BorderColor string `json:"borderColor,omitempty"`
+
+	// Border width of label.
+	BorderWidth int `json:"borderWidth,omitempty"`
+
+	// Border radius of label.
+	BorderRadius int `json:"borderRadius,omitempty"`
+
+	// Border type of label.
+	// Options: 'solid', 'dashed', 'dotted'
+	BorderType string `json:"borderType,omitempty"`
+
+	// Border dash offset of label.
+	BorderDashOffset int `json:"borderDashOffset,omitempty"`
+
+	// Shadow blur of text block.
+	ShadowBlur int `json:"shadowBlur,omitempty"`
+
+	// Shadow color of text block.
+	ShadowColor string `json:"shadowColor,omitempty"`
+
+	// Shadow X offset of text block.
+	ShadowOffsetX int `json:"shadowOffsetX,omitempty"`
+
+	// Shadow Y offset of text block.
+	ShadowOffsetY int `json:"shadowOffsetY,omitempty"`
+
+	// Padding title space around content. See legend.textStyle.padding
+	// The unit is px. Default values for each position are 5.
+	// And they can be set to different values with left, right, top, and bottom.
+	Padding interface{} `json:"padding,omitempty"`
+
+	// Width of text block.
+	Width int `json:"width,omitempty"`
+
+	// Height of text block.
+	Height int `json:"height,omitempty"`
+
+	// Text border color.
+	TextBorderColor string `json:"textBorderColor,omitempty"`
+
+	// Text border width.
+	TextBorderWidth int `json:"textBorderWidth,omitempty"`
+
+	// Text border type
+	// Options: 'solid', 'dashed', 'dotted'
+	TextBorderType string `json:"textBorderType,omitempty"`
+
+	// Text border dash offset.
+	TextBorderDashOffset int `json:"textBorderDashOffset,omitempty"`
+
+	// Text shadow color.
+	TextShadowColor string `json:"textShadowColor,omitempty"`
+
+	// Text shadow blur.
+	TextShadowBlur int `json:"textShadowBlur,omitempty"`
+
+	// Text shadow X offset.
+	TextShadowOffsetX int `json:"textShadowOffsetX,omitempty"`
+
+	// Text shadow Y offset.
+	TextShadowOffsetY int `json:"textShadowOffsetY,omitempty"`
+
+	// Determine how to display the text when it's overflow. Available when width is set.
+	//
+	// 'truncate' Truncate the text and trailing with ellipsis.
+	// 'break' Break by word
+	// 'breakAll' Break by character.
+	Overflow string `json:"overflow,omitempty"`
+
+	// Ellipsis
+	Ellipsis types.Bool `json:"ellipsis,omitempty"`
+
 	// The content formatter of value
 	//
 	// 1. String template
@@ -546,9 +665,6 @@ type Detail struct {
 	// The format of callback function:
 	// (value: number) => string
 	Formatter types.FuncStr `json:"formatter,omitempty"`
-
-	// Font size of the value in px
-	FontSize int `json:"fontSize,omitempty"`
 
 	// Value position relative to the center of chart
 	// OffceCenter is provided as [x, y] where x and y are either a number (px, provided
