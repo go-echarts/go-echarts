6db5b8ba80e05f80504daac93093071ef2927b72
sunyihoo <627077106@qq.com>
2024-11-04 19:23:37 +0800

    update(opt): Add new fields to the TextStyle/Progress/Detail structure. (#497)
    ---------
    
    Co-authored-by: sunyihao <sunyihoo@163.com>
    Co-authored-by: Koy Zhuang <koy@ko8e24.top>

--- opts/text_style.go
+++ opts/text_style.go
@@ -1,5 +1,7 @@
 package opts
 
+import "github.com/go-echarts/go-echarts/v2/types"
+
 // TextStyle is the option set for a text style component.
 type TextStyle struct {
 	// Font color
@@ -24,6 +26,89 @@ type TextStyle struct {
 
 	// Font size
 	FontSize int `json:"fontSize,omitempty"`
+
+	// Width of text block.
+	Width int `json:"width,omitempty"`
+
+	// Height of text block.
+	Height int `json:"height,omitempty"`
+
+	// Horizontal alignment of text, automatic by default.
+	// Options: 'left', 'center', 'right'
+	Align string `json:"align,omitempty"`
+
+	// Vertical alignment of text, automatic by default.
+	// Options: 'top', 'middle', 'bottom'
+	VerticalAlign string `json:"verticalAlign,omitempty"`
+
+	// Line height of the text fragment.
+	LineHeight int `json:"lineHeight,omitempty"`
+
+	// Text border color.
+	TextBorderColor string `json:"textBorderColor,omitempty"`
+
+	// Text border width.
+	TextBorderWidth int `json:"textBorderWidth,omitempty"`
+
+	// Text border type
+	// Options: 'solid', 'dashed', 'dotted'
+	TextBorderType string `json:"textBorderType,omitempty"`
+
+	// Text border dash offset.
+	TextBorderDashOffset int `json:"textBorderDashOffset,omitempty"`
+
+	// Text shadow color.
+	TextShadowColor string `json:"textShadowColor,omitempty"`
+
+	// Text shadow blur.
+	TextShadowBlur int `json:"textShadowBlur,omitempty"`
+
+	// Text shadow X offset.
+	TextShadowOffsetX int `json:"textShadowOffsetX,omitempty"`
+
+	// Text shadow Y offset.
+	TextShadowOffsetY int `json:"textShadowOffsetY,omitempty"`
+
+	// Determine how to display the text when it's overflow. Available when width is set.
+	//
+	// 'truncate' Truncate the text and trailing with ellipsis.
+	// 'break' Break by word
+	// 'breakAll' Break by character.
+	Overflow string `json:"overflow,omitempty"`
+
+	// Ellipsis
+	Ellipsis types.Bool `json:"ellipsis,omitempty"`
+
+	// Background color of label, which is transparent by default.
+	BackgroundColor string `json:"backgroundColor,omitempty"`
+
+	// Border color of label.
+	BorderColor string `json:"borderColor,omitempty"`
+
+	// Border width of label.
+	BorderWidth int `json:"borderWidth,omitempty"`
+
+	// Border radius of label.
+	BorderRadius int `json:"borderRadius,omitempty"`
+
+	// Border type of label.
+	// Options: 'solid', 'dashed', 'dotted'
+	BorderType string `json:"borderType,omitempty"`
+
+	// Border dash offset of label.
+	BorderDashOffset int `json:"borderDashOffset,omitempty"`
+
+	// Shadow blur of text block.
+	ShadowBlur int `json:"shadowBlur,omitempty"`
+
+	// Shadow color of text block.
+	ShadowColor string `json:"shadowColor,omitempty"`
+
+	// Shadow X offset of text block.
+	ShadowOffsetX int `json:"shadowOffsetX,omitempty"`
+
+	// Shadow Y offset of text block.
+	ShadowOffsetY int `json:"shadowOffsetY,omitempty"`
 
 	// Padding title space around content. See legend.textStyle.padding
 	// The unit is px. Default values for each position are 5.
