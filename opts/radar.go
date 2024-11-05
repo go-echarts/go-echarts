d386dfbeba8e4e0b6b0ebe0588f0cf86eb5ef736
Sam Randall <12263933+a-sam-randall@users.noreply.github.com>
2024-11-06 00:14:10 +1100

    update(opts): add radar axisName option (#500)
    ---------
    
    Co-authored-by: Koy Zhuang <koy@ko8e24.top>

--- opts/radar.go
+++ opts/radar.go
@@ -1,5 +1,7 @@
 package opts
 
+import "github.com/go-echarts/go-echarts/v2/types"
+
 // RadarComponent is the option set for a radar component.
 // https://echarts.apache.org/en/option.html#radar
 type RadarComponent struct {
@@ -28,6 +30,9 @@ type RadarComponent struct {
 
 	// The start angle of coordinate, which is the angle of the first indicator axis.
 	StartAngle float64 `json:"startAngle,omitempty"`
+
+	// Name options for radar indicators.
+	AxisName *AxisName `json:"axisName,omitempty"`
 }
 
 // Indicator is the option set for a radar chart.
@@ -43,4 +48,116 @@ type Indicator struct {
 
 	// Specify a color the indicator.
 	Color string `json:"color,omitempty"`
+}
+
+// Name options for radar indicators.
+type AxisName struct {
+	// Whether to display the indicator's name.
+	Show types.Bool `json:"show,omitempty"`
+
+	// The formatter of indicator's name, using string template, the template variable should be the indicator's name {value}
+	Formatter types.FuncStr `json:"formatter,omitempty"`
+
+	// Font color
+	Color string `json:"color,omitempty"`
+
+	// Font style
+	// Options: 'normal', 'italic', 'oblique'
+	FontStyle string `json:"fontStyle,omitempty"`
+
+	// FontWeight main title font thick weight.
+	// Options are:
+	// 'normal'
+	// 'bold'
+	// 'bolder'
+	// 'lighter'
+	// 100 | 200 | 300 | 400...
+	FontWeight string `json:"fontWeight,omitempty"`
+
+	// Font family the main title font family.
+	// Options: "sans-serif", 'serif' , 'monospace', 'Arial', 'Courier New', 'Microsoft YaHei', ...
+	FontFamily string `json:"fontFamily,omitempty"`
+
+	// Font size
+	FontSize int `json:"fontSize,omitempty"`
+
+	// Width of text block.
+	Width int `json:"width,omitempty"`
+
+	// Height of text block.
+	Height int `json:"height,omitempty"`
+
+	// Line height of the text fragment.
+	LineHeight int `json:"lineHeight,omitempty"`
+
+	// Text border color.
+	TextBorderColor string `json:"textBorderColor,omitempty"`
+
+	// Text border width.
+	TextBorderWidth int `json:"textBorderWidth,omitempty"`
+
+	// Text border type
+	// Options: 'solid', 'dashed', 'dotted'
+	TextBorderType string `json:"textBorderType,omitempty"`
+
+	// Text border dash offset.
+	TextBorderDashOffset int `json:"textBorderDashOffset,omitempty"`
+
+	// Text shadow color.
+	TextShadowColor string `json:"textShadowColor,omitempty"`
+
+	// Text shadow blur.
+	TextShadowBlur int `json:"textShadowBlur,omitempty"`
+
+	// Text shadow X offset.
+	TextShadowOffsetX int `json:"textShadowOffsetX,omitempty"`
+
+	// Text shadow Y offset.
+	TextShadowOffsetY int `json:"textShadowOffsetY,omitempty"`
+
+	// Determine how to display the text when it's overflow. Available when width is set.
+	//
+	// 'truncate' Truncate the text and trailing with ellipsis.
+	// 'break' Break by word
+	// 'breakAll' Break by character.
+	Overflow string `json:"overflow,omitempty"`
+
+	// Ellipsis
+	Ellipsis types.Bool `json:"ellipsis,omitempty"`
+
+	// Background color of label, which is transparent by default.
+	BackgroundColor string `json:"backgroundColor,omitempty"`
+
+	// Border color of label.
+	BorderColor string `json:"borderColor,omitempty"`
+
+	// Border width of label.
+	BorderWidth int `json:"borderWidth,omitempty"`
+
+	// Border radius of label.
+	BorderRadius int `json:"borderRadius,omitempty"`
+
+	// Border type of label.
+	// Options: 'solid', 'dashed', 'dotted'
+	BorderType string `json:"borderType,omitempty"`
+
+	// Border dash offset of label.
+	BorderDashOffset int `json:"borderDashOffset,omitempty"`
+
+	// Shadow blur of text block.
+	ShadowBlur int `json:"shadowBlur,omitempty"`
+
+	// Shadow color of text block.
+	ShadowColor string `json:"shadowColor,omitempty"`
+
+	// Shadow X offset of text block.
+	ShadowOffsetX int `json:"shadowOffsetX,omitempty"`
+
+	// Shadow Y offset of text block.
+	ShadowOffsetY int `json:"shadowOffsetY,omitempty"`
+
+	// Padding title space around content. See legend.textStyle.padding
+	// The unit is px. Default values for each position are 5.
+	// And they can be set to different values with left, right, top, and bottom.
+	Padding interface{} `json:"padding,omitempty"`
 }
