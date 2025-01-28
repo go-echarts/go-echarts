448634c2902fea26651f6fe30410a084e5aa5016
Koy Zhuang <koy@ko8e24.top>
2025-01-28 11:50:46 +0800

    chore(render): remove unused isset in render (#517)

--- render/engine.go
+++ render/engine.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"reflect"
 	"regexp"
 	"strings"
 
@@ -37,29 +36,12 @@ type Renderer interface {
 	RenderSnippet() ChartSnippet
 }
 
-// isSet check if the field exist in the chart instance
-// Shamed copy from https://stackoverflow.com/questions/44675087/golang-template-variable-isset
-func isSet(name string, data interface{}) bool {
-	v := reflect.ValueOf(data)
-
-	if v.Kind() == reflect.Ptr {
-		v = v.Elem()
-	}
-
-	if v.Kind() != reflect.Struct {
-		return false
-	}
-
-	return v.FieldByName(name).IsValid()
-}
-
 // MustTemplate creates a new template with the given name and parsed contents.
 func MustTemplate(name string, contents []string) *template.Template {
 	tpl := template.New(name).Funcs(template.FuncMap{
 		"safeJS": func(s interface{}) template.JS {
 			return template.JS(fmt.Sprint(s))
 		},
-		"isSet": isSet,
 		"injectInstance": func(funcStr types.FuncStr, echartsInstancePlaceholder string, chartID string) string {
 			instance := EchartsInstancePrefix + chartID
 			return strings.Replace(string(funcStr), echartsInstancePlaceholder, instance, -1)
