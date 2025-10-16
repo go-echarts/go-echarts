d32a2d02628d141c9bc0fc52b5d90541dd86a8a6
KoyZhuang <koy@ko8e24.top>
2025-10-16 20:46:35 +0800

    fix(opts): tooltip ValueFormatter type types.FuncStr

--- opts/series.go
+++ opts/series.go
@@ -844,7 +844,7 @@ type SeriesTooltip struct {
 
 	// ValueFormatter Callback function for formatting the value section in tooltip.
 	// valueFormatter: (value) => '$' + value.toFixed(2)
-	ValueFormatter string `json:"valueFormatter,omitempty"`
+	ValueFormatter types.FuncStr `json:"valueFormatter,omitempty"`
 
 	// The content formatter of tooltip's floating layer which supports string template and callback function.
 	// See https://echarts.apache.org/en/option.html#grid.tooltip.position
