c5a16ac481c23e52d3c82ecf6dfa327efa5e4af1
Steve Kuznetsov <stekuznetsov@microsoft.com>
2025-10-17 05:17:01 -0600

    update(opt): add missing opts for sunburst (#579)
    * sunburst: add missing attributes
    
    Signed-off-by: Steve Kuznetsov <stekuznetsov@microsoft.com>
    
    * opts: allow configuring emphasis focus
    
    Signed-off-by: Steve Kuznetsov <stekuznetsov@microsoft.com>
    
    * opts: support border radius in item style
    
    Signed-off-by: Steve Kuznetsov <stekuznetsov@microsoft.com>

--- opts/series.go
+++ opts/series.go
@@ -147,6 +147,18 @@ type Emphasis struct {
 
 	// the emphasis style of item
 	ItemStyle *ItemStyle `json:"itemStyle,omitempty"`
+
+	// Focus configures the behavior of a Sunburst chart when a user interacts with the data.
+	// When the data is highlighted, whether to fade out of other data to focus the highlighted.
+	// The following configurations are supported:
+	//   'none' Do not fade out other data, it's by default.
+	//   'self' Only focus (not fade out) the element of the currently highlighted data.
+	//   'series' Focus on all elements of the series which the currently highlighted data belongs to.
+	//   'ancestor' Focus on all ancestor nodes.
+	//   'descendant' Focus on all descendants nodes.
+	//   'relative' Focus on all ancestor and descendants nodes. (Since v5.6.0)
+	// https://echarts.apache.org/en/option.html#series-sunburst.emphasis.focus
+	Focus string `json:"focus,omitempty"`
 }
 
 // Animation represents animation behaviors of series.
@@ -172,6 +184,9 @@ type ItemStyle struct {
 
 	// Geo area filling color
 	AreaColor string `json:"areaColor,omitempty"`
+
+	// BorderRadius configures the radius of items in the chart.
+	BorderRadius string `json:"borderRadius,omitempty"`
 
 	// BorderColor is the hart border color
 	// Kline  Up candle border color
