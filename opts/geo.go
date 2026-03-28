7e2903a9a2c01bf71d6f0ff59006e1425642036d
westo0852 <140355250+westo0852@users.noreply.github.com>
2026-03-29 08:44:55 +1100

    geo: center, zoom and roam opts
    Added more Geo options from the ECharts API that were missing:
    [see docs](https://echarts.apache.org/en/option.html#geo)

--- opts/geo.go
+++ opts/geo.go
@@ -8,6 +8,27 @@ type GeoComponent struct {
 	// Map charts.
 	Map string `json:"map,omitempty"`
 
+	// Specifies which point on the map should be placed at center of viewport.
+	// When roaming, values in center and zoom will be modified correspondingly.
+	// Center is in longitude and latitude by default. Use the projected coordinates.
+	// Example: [115.97, 29.71].
+	// A percentage string can also be used, like '30%', based on the bounding rect.
+	// You can use '0%' to place the top or left of bounding rect to the center,
+	// or use '50%' to place the entire source map at the the center of the viewport.
+	// Example: [115, '30%'].
+	Center interface{} `json:"center,omitempty"`
+
+	// Zoom rate of current viewport. The value less than 1 indicates zooming out,
+	// while the value greater than 1 indicates zooming in.
+	// When roaming, values in center and zoom will be modified correspondingly.
+	Zoom types.Float `json:"zoom,omitempty"`
+
+	// Whether to enable mouse or touch roam (move and zoom):
+	// * false: roam is disabled.
+	// * true: both zoom and move (translation) are available.
+	// When roaming, values in center and zoom will be modified correspondingly.
+	Roam types.Bool `json:"roam,omitempty"`
+
 	// Graphic style of Map Area Border, emphasis is the style when it is highlighted,
 	// like being hovered by mouse, or highlighted via legend connect.
 	ItemStyle *ItemStyle `json:"itemStyle,omitempty"`
